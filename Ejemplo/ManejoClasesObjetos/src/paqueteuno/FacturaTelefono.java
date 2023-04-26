eae5cc1cf13fc7fd12e1ef754b17673bd468cd48
augusto20052008 <augustovdavilar@gmail.com>
2023-04-26 17:18:01 -0500

    Presentar

--- Ejemplo/ManejoClasesObjetos/src/paqueteuno/FacturaTelefono.java
+++ Ejemplo/ManejoClasesObjetos/src/paqueteuno/FacturaTelefono.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paqueteuno;
 
-/**
- *
- * @author reroes
- */
 public class FacturaTelefono {
     private String numeroTelefono;
     private double minutosMes;
@@ -16,36 +8,38 @@ public class FacturaTelefono {
     private double valorFactura;
     
     public void establecerNumeroTelefono(String c){
-        numeroTelefono = c;
+        this.numeroTelefono = c;
     }
     
     public void establecerMinutosMes(double c){
-        minutosMes = c;
+        this.minutosMes = c;
     }
     
     public void establecerValorMinuto(double c){
-        valorMinuto = c;
+        this.valorMinuto = c;
     }
     
     // public void establecerValorFactura(){
     public void calcularValorFactura(){
-        valorFactura = minutosMes * valorMinuto;
+        this.valorFactura = this.minutosMes * this.valorMinuto;
     }
-    
+    public void establecerValorFactura(double valorFactura){
+        this.valorFactura = valorFactura;
+    }
     public String obtenerNumeroTelefono(){
-        return numeroTelefono;
+        return this.numeroTelefono;
     }
     
     public double obtenerMinutosMes(){
-        return minutosMes;
+        return this.minutosMes;
     }
     
     public double obtenerValorMinuto(){
-        return valorMinuto;
+        return this.valorMinuto;
     }
     
     public double obtenerValorFactura(){
-        return valorFactura;
+        return this.valorFactura;
     }
     
     
